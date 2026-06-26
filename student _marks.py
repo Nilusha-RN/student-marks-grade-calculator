6252dcef8086e332c030b5c3bdcb914961472334
Nilusha-RN <nilushanirmal883@gmail.com>
2026-06-27 01:14:02 +0530

    Add marks for three subjects

--- student _marks.py
+++ student _marks.py
@@ -1,5 +1,10 @@
 name = input("Enter student name: ")
-mark = int(input("Enter your mark: "))
+
+python_mark = int(input("Enter Python mark: "))
+database_mark = int(input("Enter Database mark: "))
+web_mark = int(input("Enter Web Development mark: "))
 
 print("\nStudent Name:", name)
-print("Mark:", mark)
+print("Python:", python_mark)
+print("Database:", database_mark)
+print("Web Development:", web_mark)
