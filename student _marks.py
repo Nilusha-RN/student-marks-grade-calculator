4dd1bd1c87025d5b255b43976b516606d4cb053b
Nilusha-RN <nilushanirmal883@gmail.com>
2026-06-30 08:21:23 +0530

    Add grade calculation

--- student _marks.py
+++ student _marks.py
@@ -7,6 +7,18 @@ web_mark = int(input("Enter Web Development mark: "))
 total = python_mark + database_mark + web_mark
 average = total / 3
 
+if average >= 75:
+    grade = "A"
+elif average >= 65:
+    grade = "B"
+elif average >= 55:
+    grade = "C"
+elif average >= 40:
+    grade = "S"
+else:
+    grade = "F"
+
 print("\nStudent Name:", name)
 print("Total Marks:", total)
 print("Average:", average)
+print("Grade:", grade)
