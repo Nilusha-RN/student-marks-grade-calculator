2c2494a5c3e632c0eba36e696a1e9c8ad21c97d1
Nilusha-RN <nilushanirmal883@gmail.com>
2026-07-02 07:59:27 +0530

    Add main menu

--- student _marks.py
+++ student _marks.py
@@ -1,30 +1,46 @@
-name = input("Enter student name: ")
+while True:
+    print("\n=== Student Marks Calculator ===")
+    print("1. Calculate Student Result")
+    print("2. Exit")
 
-python_mark = int(input("Enter Python mark: "))
-database_mark = int(input("Enter Database mark: "))
-web_mark = int(input("Enter Web Development mark: "))
+    choice = input("Enter your choice: ")
 
-total = python_mark + database_mark + web_mark
-average = total / 3
+    if choice == "1":
+        name = input("Enter student name: ")
 
-if average >= 75:
-    grade = "A"
-elif average >= 65:
-    grade = "B"
-elif average >= 55:
-    grade = "C"
-elif average >= 40:
-    grade = "S"
-else:
-    grade = "F"
+        python_mark = int(input("Enter Python mark: "))
+        database_mark = int(input("Enter Database mark: "))
+        web_mark = int(input("Enter Web Development mark: "))
 
-if python_mark < 40 or database_mark < 40 or web_mark < 40:
-    result = "Fail"
-else:
-    result = "Pass"
+        total = python_mark + database_mark + web_mark
+        average = total / 3
 
-print("\nStudent Name:", name)
-print("Total Marks:", total)
-print("Average:", average)
-print("Grade:", grade)
-print("Result:", result)
+        if average >= 75:
+            grade = "A"
+        elif average >= 65:
+            grade = "B"
+        elif average >= 55:
+            grade = "C"
+        elif average >= 40:
+            grade = "S"
+        else:
+            grade = "F"
+
+        if python_mark < 40 or database_mark < 40 or web_mark < 40:
+            result = "Fail"
+        else:
+            result = "Pass"
+
+        print("\n--- Student Result ---")
+        print("Name:", name)
+        print("Total:", total)
+        print("Average:", average)
+        print("Grade:", grade)
+        print("Result:", result)
+
+    elif choice == "2":
+        print("Program closed.")
+        break
+
+    else:
+        print("Invalid choice.")
